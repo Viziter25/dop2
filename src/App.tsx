9b7378708b92392b068d4ab51f5533f7ab75035c
Zlotnik-Ilya <ilya.zlotnik@mail.ru>
2022-10-02 13:44:37 +0300

    feat: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+type PropsType = {
+  userId: number, id: number, title: string, completed: boolean
+}
+
 function App() {
+  const [todos, setTodos] = useState<PropsType[]>([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setTodos(json))
+  }, [])
+
+  const onclickHandler = () => {
+    setTodos([])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onclickHandler}>new posts</button>
+      <ul>
+        {
+          todos.map(todo => {
+            return (
+              <li>{todo.id}- {todo.title}</li>
+            )
+          })
+        }
+      </ul>
+
+
     </div>
   );
 }
+
 
 export default App;
